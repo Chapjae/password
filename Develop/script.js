6b5897cac26508970dcd47abcc9cca5b9c14a683
Jay Liu <jayliu89@gmail.com>
2023-06-28 20:09:44 -0700

    started working on password generation

--- Develop/script.js
+++ Develop/script.js
@@ -1,49 +1,57 @@
 // Assignment code here
 // let specialChar = ["!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","/","]","^",]
-let specialChar = "\"\ !#$%&'()*+,-./:;<=>?@[]^_`{|}~"
-let upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-let lowerChar = "abcdefghijklmnopqrstuvwxyz"
-let numbers = "1234567890"
-
-console.log(specialChar)
+var specialChar = "\"\ !#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowerChar = "abcdefghijklmnopqrstuvwxyz"
+var numbers = "1234567890"
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 generatePassword = () => {
-  var passwordText = []
+  var passwordText = [];
   
-  let length = prompt("Choose a password length between 8 and 128");
-  let stringParse = parseFloat(length);
+  var length = prompt("Choose a password length between 8 and 128");
+  var lengthParse = parseInt(length)
+  
+  var specialChar = confirm ("Would you like to use special characters?")
+  var upperChar = confirm ("Would you like to use uppercase characters?")
+  var lowerChar = confirm ("Would you like to use lowercase characters?")
+  var numberChar = confirm ("Would you like to use numbers?")
+  
   // let confirmSpecial = confirm("Would you like to use special characters?")
-  let specialChar = confirm ("Would you like to use special characters?")
-  let upperChar = confirm ("Would you like to use uppercase characters?")
-  let lowerChar = confirm ("Would you like to use lowercase characters?")
-  let numberChar = confirm ("Would you like to use numbers?")
-  if (stringParse >= 8 && stringParse < 128) { 
-    passwordText.length = stringParse;
+  
+  // might want to refactor as a function that passes in the character strings that takes in ppassword.text as an arguement and runs rng as a funtion" 
+  if (lengthParse >= 8 && lengthParse <= 128) {
+      specialChar
+      upperChar
+      lowerChar
+      numberChar
     } else {
       alert("Your password must be between 8 and 128 characters!")
       }
-
-  if (specialChar == true) {
-    passwordText.push(specialChar[Math.floor(Math.random() * specialChar.length)])
-    } 
   
+  
+  if (specialChar == true) {
+      passwordText.push(specialChar[Math.floor(Math.random() * specialChar.length)])
+    }
+
   if (upperChar == true) {
       passwordText.push(upperChar[Math.floor(Math.random() * upperChar.length)])
-    }  
-  
-  if (lowerChar == true) {
-      passwordText.push(lowerChar.specialChar[Math.floor(Math.random() * lowerChar.length)])
-    } 
-  
-  if (numberChar == true) {
-      passwordText.push(numbers[Math.floor(Math.random() * numbers.length)])
     }
+      
+  if (lowerChar == true) {
+        passwordText.push(lowerChar[Math.floor(Math.random() * lowerChar.length)])
+      } 
+    
+  if (numberChar == true) {
+        passwordText.push(numberChar[Math.floor(Math.random() * numberChar.length)])
+      }
   
-  
-  
+      debugger
+  return passwordText
+          
   
 }
 // Write password to the #password input
