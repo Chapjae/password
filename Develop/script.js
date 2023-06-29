a0297d1440cce03ee5edf865e21bb2eac77eef1e
Jay Liu <jayliu89@gmail.com>
2023-06-29 00:45:10 -0700

    created confirmCharacters function to handle confirm windows and if statements for them. generatePassword function only handles password length if statement now. might stay with this formatting, as the functions might increase readability

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,7 @@ var specialChar = "\"\ !#$%&'()*+,-./:;<=>?@[]^_`{|}~"
 var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var lowerChar = "abcdefghijklmnopqrstuvwxyz"
 var numbers = "1234567890"
-
+var passwordText = '';
  
   
 // Get references to the #generate element
@@ -15,42 +15,42 @@ function randomSelector(str) {
   var randomChar = str.charAt(randomIdx);
   return randomChar;
 }
+function confirmCharacters(specialCharConfirm, upperCharConfirm, lowerCharConfirm, numberCharConfirm) {
+  var specialCharConfirm = confirm ("Would you like to use special characters?")
+  var upperCharConfirm = confirm ("Would you like to use uppercase characters?")
+  var lowerCharConfirm = confirm ("Would you like to use lowercase characters?")
+  var numberCharConfirm = confirm ("Would you like to use numbers?")   
+  
+  if (specialCharConfirm == true) {
+      passwordText += randomSelector(specialChar)
+      if (upperCharConfirm == true) {
+        passwordText += randomSelector(upperChar)
+        if (lowerCharConfirm == true) {
+          passwordText += randomSelector(lowerChar)
+          if (numberCharConfirm == true) {
+            passwordText += randomSelector(numbers)
+            } else {
+            alert("Please choose at least one type of character.")
+          }
+        } 
+      }
+    }
+}
 
 function generatePassword() {
-  
   var length = prompt("Choose a password length between 8 and 128");
   var lengthParse = parseInt(length)
   
   
-  var specialCharConfirm = confirm ("Would you like to use special characters?")
-  var upperCharConfirm = confirm ("Would you like to use uppercase characters?")
-  var lowerCharConfirm = confirm ("Would you like to use lowercase characters?")
-  var numberCharConfirm = confirm ("Would you like to use numbers?")
   
+ 
   if (lengthParse >= 8 && lengthParse <= 128) {
-    var passwordText = '';
+    confirmCharacters()
     } else {
       alert("Your password must be between 8 and 128 characters!")
         }
  
-  if (specialCharConfirm == true) {
-    passwordText += randomSelector(specialChar)
-    }
-  if (upperCharConfirm == true) {
-    passwordText += randomSelector(upperChar)
-    }
-  if (lowerCharConfirm == true) {
-    passwordText += randomSelector(lowerChar)
-    }
-  if (numberCharConfirm == true) {
-    passwordText += randomSelector(numbers)
-    } else {
-      alert("Please choose at least one type of character.")
-    }
-
-      
-    passwordText.valueOf()
-
+        
   return passwordText 
 }
 // Write password to the #password input
