0d16fd38c32d0494335d0e909e0889e52d01518f
Jay Liu <jayliu89@gmail.com>
2023-06-28 22:07:48 -0700

    created randomSelector function, currently having an error with charAt

--- Develop/script.js
+++ Develop/script.js
@@ -5,54 +5,60 @@ var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var lowerChar = "abcdefghijklmnopqrstuvwxyz"
 var numbers = "1234567890"
 
-
+ 
+  
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-generatePassword = () => {
-  var passwordText = [];
+function randomSelector(str) {
+  var randomIdx = Math.floor(Math.random() * str.length);
+  var randomChar = str.charAt(randomIdx);
+  return randomChar;
+}
+
+function generatePassword() {
   
   var length = prompt("Choose a password length between 8 and 128");
   var lengthParse = parseInt(length)
+  var passwordText = '';
   
-  var specialChar = confirm ("Would you like to use special characters?")
-  var upperChar = confirm ("Would you like to use uppercase characters?")
-  var lowerChar = confirm ("Would you like to use lowercase characters?")
-  var numberChar = confirm ("Would you like to use numbers?")
-  
-  // let confirmSpecial = confirm("Would you like to use special characters?")
-  
-  // might want to refactor as a function that passes in the character strings that takes in ppassword.text as an arguement and runs rng as a funtion" 
+  var specialCharConfirm = confirm ("Would you like to use special characters?")
+  var upperCharConfirm = confirm ("Would you like to use uppercase characters?")
+  var lowerCharConfirm = confirm ("Would you like to use lowercase characters?")
+  var numberCharConfirm = confirm ("Would you like to use numbers?")
+ 
   if (lengthParse >= 8 && lengthParse <= 128) {
-      specialChar
-      upperChar
-      lowerChar
-      numberChar
-    } else {
+    if (specialCharConfirm == true) {
+      randomSelector(specialChar)
+      }
+    if (upperCharConfirm == true) {
+      randomSelector(upperChar)
+      }
+    if (lowerCharConfirm == true) {
+      randomSelector(lowerChar)
+      }
+    if (numberCharConfirm == true) {
+      randomSelector(numbers)
+      }
+  } else {
       alert("Your password must be between 8 and 128 characters!")
       }
-  
-  
-  if (specialChar == true) {
-      passwordText.push(specialChar[Math.floor(Math.random() * specialChar.length)])
-    }
 
-  if (upperChar == true) {
-      passwordText.push(upperChar[Math.floor(Math.random() * upperChar.length)])
-    }
+  
+
+  // if (upperChar == true) {
+  //     passwordText += upperChar[Math.floor(Math.random() * upperChar.length)]
+  //   }
       
-  if (lowerChar == true) {
-        passwordText.push(lowerChar[Math.floor(Math.random() * lowerChar.length)])
-      } 
+  // if (lowerChar == true) {
+  //       passwordText.push(lowerChar[Math.floor(Math.random() * lowerChar.length)])
+  //     } 
     
-  if (numberChar == true) {
-        passwordText.push(numberChar[Math.floor(Math.random() * numberChar.length)])
-      }
-  
-      debugger
-  return passwordText
+  // if (numberChar == true) {
+  //       passwordText.push(numberChar[Math.floor(Math.random() * numberChar.length)])
+  //     }
           
-  
+  return passwordText
 }
 // Write password to the #password input
 function writePassword() {
