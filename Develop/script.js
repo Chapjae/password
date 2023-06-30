4038fa0a1c8d05f6d779bed805e10972e94d3497
Jay Liu <jayliu89@gmail.com>
2023-06-29 23:50:09 -0700

    alerts corrected, if statements refactored. added textChosen variable

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,6 @@ var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var lowerChar = "abcdefghijklmnopqrstuvwxyz"
 var numbers = "1234567890"
 var everyChar = "\"\ !#$%&'()*+,-./:;<=>?@[]^_`{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
-var passwordText = '';
  
   
 // Get references to the #generate element
@@ -19,35 +18,39 @@ function randomSelector(str) {
 
 function generatePassword() {
   var passwordText = ""
+  var textChosen = ""
   var passwordLength = prompt("Choose a password length between 8 and 128");
   var lengthParse = parseInt(passwordLength)
+ 
+  if (lengthParse < 8 || lengthParse > 128) {
+    alert("Your password must be between 8 and 128 characters!")
+    return;
+    } else {
+    var specialCharConfirm = confirm ("Would you like to use special characters?")
+    var upperCharConfirm = confirm ("Would you like to use uppercase characters?")
+    var lowerCharConfirm = confirm ("Would you like to use lowercase characters?")
+    var numberCharConfirm = confirm ("Would you like to use numbers?")   
 
-  var specialCharConfirm = confirm ("Would you like to use special characters?")
-  var upperCharConfirm = confirm ("Would you like to use uppercase characters?")
-  var lowerCharConfirm = confirm ("Would you like to use lowercase characters?")
-  var numberCharConfirm = confirm ("Would you like to use numbers?")   
-
-      if (lengthParse < 8 || lengthParse > 128) {
-        alert("Your password must be between 8 and 128 characters!")
-        return;
-          } else {
-            if (specialCharConfirm) {
-            passwordText += randomSelector(specialChar)
-            if (upperCharConfirm) {
-            passwordText += randomSelector(upperChar)
-            if (lowerCharConfirm) {
-            passwordText += randomSelector(lowerChar)
-            if (numberCharConfirm) {
-            passwordText += randomSelector(numbers)
-              }
-             for (var i = passwordText; i < lengthParse; i++) {
-              var passwordFill = randomSelector(everyChar)
-              passwordFill += passwordText
-              }
+      if (specialCharConfirm) {
+        passwordText += randomSelector(specialChar)
+        textChosen += specialChar
+      if (upperCharConfirm) {
+        passwordText += randomSelector(upperChar)
+        textChosen += upperChar
+      if (lowerCharConfirm) {
+        passwordText += randomSelector(lowerChar)
+        textChosen += lowerChar
+      if (numberCharConfirm) {
+        passwordText += randomSelector(numbers)
+        textChosen += numbers    
+          }
+          for (var i = 0; i < lengthParse; i++) {
+          passwordText += randomSelector(textChosen)
             }
-          } 
-        }
+          }
+        } 
       }
+    }
   return passwordText;
 }
 // Write password to the #password input
